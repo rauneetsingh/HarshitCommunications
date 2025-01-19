d16bbe1a975ffd9458fc81369b0a44499f6b2b63
Rauneet Singh <rauneet.03@outlook.com>
2025-01-19 21:43:41 +0530

    CategoryAdded

---
+++ HarshitCommunications/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HarshitCommunications.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be in between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
