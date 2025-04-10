5f23ecb91daa93c1f4177989fa332fd947252572
Rauneet Singh <rauneet.03@outlook.com>
2025-04-10 13:13:05 +0530

    usersecrets

--- HarshitCommunications/Program.cs
+++ HarshitCommunications/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Configuration.AddUserSecrets<Program>();
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.ConfigureApplicationCookie(options =>
 {
